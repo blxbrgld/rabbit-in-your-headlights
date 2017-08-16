0ee1794417ddd465c73854eac3ac05c15e39344e
npapadopoulos <npapadopoulos@cognity.gr>
2017-08-16 11:46:28 +0300

    Exchanges + Queues Should Be Declared Dynamically

--- src/main/java/gr/blxbrgld/rabbit/configuration/MessagingConfiguration.java
+++ src/main/java/gr/blxbrgld/rabbit/configuration/MessagingConfiguration.java
@@ -1,7 +1,5 @@
 package gr.blxbrgld.rabbit.configuration;
 
-import gr.blxbrgld.rabbit.utils.Constants;
-import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitManagementTemplate;
@@ -26,37 +24,20 @@ public class MessagingConfiguration {
     private String password;
 
     @Bean
-    public Queue queue() {
-        return new Queue(Constants.QUEUE_NAME, false); //durable = false, No Need To Survive Broker Restarts
-    }
-
-    @Bean
-    public TopicExchange exchange() {
-        return new TopicExchange(Constants.EXCHANGE_NAME);
-    }
-
-    @Bean
-    public Binding binding(Queue queue, TopicExchange topicExchange) {
-        return BindingBuilder
-            .bind(queue)
-            .to(topicExchange)
-            .with(Constants.QUEUE_NAME);
-    }
-
-    @Bean
     public RabbitManagementTemplate managementTemplate() { //Construct A Template for "http://username:password@host:port/api/"
-        String uri = new StringBuilder()
-            .append("http://")
-            .append(username)
-            .append(":")
-            .append(password)
-            .append("@")
-            .append(host)
-            .append(":")
-            .append(port)
-            .append("/api/")
-            .toString();
-        return new RabbitManagementTemplate(uri);
+        return new RabbitManagementTemplate(
+            new StringBuilder()
+                .append("http://")
+                .append(username)
+                .append(":")
+                .append(password)
+                .append("@")
+                .append(host)
+                .append(":")
+                .append(port)
+                .append("/api/")
+                .toString()
+        );
     }
 
     @Bean
